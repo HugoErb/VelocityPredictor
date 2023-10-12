b77fb6ec3b1411e51db208b0a0a5410f76027623
HugoErb <erb.hugo@yahoo.fr>
2023-10-12 23:30:46 +0200

    adpat fake data + add polynomial regression

--- create_fake_data.py
+++ create_fake_data.py
@@ -15,8 +15,8 @@ jours_homme_dispo = np.random.randint(NB_PERSONNES, NB_PERSONNES*MAX_JOURS + 1, 
 
 # Logique pour créer une vélocité réelle en fonction du nombre de jours homme disponibles
 # Vélocité = 2 fois le nombre de jours homme + variation aléatoire
-variation_aleatoire = np.random.uniform(0.7, 1.3, NB_SPRINTS)
-velocite_reelle = jours_homme_dispo * (2 * variation_aleatoire)
+variation_aleatoire = np.random.uniform(0.85, 1.15, NB_SPRINTS)
+velocite_reelle = jours_homme_dispo * variation_aleatoire
 
 # Création du DataFrame
 df = pd.DataFrame({"jours_homme_dispo": jours_homme_dispo, "velocite_reelle": velocite_reelle})
--- predictor.py
+++ predictor.py
@@ -2,6 +2,7 @@ import duckdb
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -18,30 +19,48 @@ X = df[['jours_homme_dispo']]
 y = df['velocite_reelle']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
 
-# Entraînement
-model = LinearRegression()
-model.fit(X_train, y_train)
+# Transformation des données pour la régression polynomiale
+poly = PolynomialFeatures(degree=2)
+X_train_poly = poly.fit_transform(X_train)
+X_test_poly = poly.transform(X_test)
+
+# Entraînement avec regréssion linéaire
+linear_model = LinearRegression()
+linear_model_poly = LinearRegression()
+linear_model.fit(X_train, y_train)
+linear_model_poly.fit(X_train_poly, y_train)
 
 # Prédictions
-test_predictions = model.predict(X_test)
+test_linear_predictions = linear_model.predict(X_test)
+test_poly_predictions = linear_model_poly.predict(X_test_poly)
 
 # Calcul des erreurs
 mean_velocity = df['velocite_reelle'].mean()
 
-mean_absolute_error_value = mean_absolute_error(y_test, test_predictions)
-mean_absolute_error_value_percent = (mean_absolute_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
+mean_linear_absolute_error_value = mean_absolute_error(y_test, test_linear_predictions)
+mean_linear_absolute_error_value_percent = (mean_linear_absolute_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
+mean_linear_squared_error_value = np.sqrt(mean_squared_error(y_test, test_linear_predictions))
+mean_linear_squared_error_value_percent = (mean_linear_squared_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
 
-mean_squared_error_value = np.sqrt(mean_squared_error(y_test, test_predictions))
-mean_squared_error_value_percent = (mean_squared_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
+mean_poly_absolute_error_value = mean_absolute_error(y_test, test_poly_predictions)
+mean_poly_absolute_error_value_percent = (mean_poly_absolute_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
+mean_poly_squared_error_value = np.sqrt(mean_squared_error(y_test, test_poly_predictions))
+mean_poly_squared_error_value_percent = (mean_poly_squared_error_value / mean_velocity) * 100 if mean_velocity != 0 else 0
 
-print(f'Vélocité moyenne = {mean_velocity}')
-print(f'Erreur absolue moyenne flat = {round(mean_absolute_error_value,2)}')
-print(f'Erreur absolue moyenne en % = {round(mean_absolute_error_value_percent,2)} %')
-print(f'Erreur quadratique moyenne flat = {round(mean_squared_error_value,2)}')
-print(f'Erreur quadratique moyenne en % = {round(mean_squared_error_value_percent,2)} %')
+print(f'Vélocité moyenne = {round(mean_velocity,2)}')
+print('')
+print(f'Erreur linéaire absolue moyenne flat = {round(mean_linear_absolute_error_value,2)}')
+print(f'Erreur linéaire absolue moyenne en % = {round(mean_linear_absolute_error_value_percent,2)} %')
+print(f'Erreur linéaire quadratique moyenne flat = {round(mean_linear_squared_error_value,2)}')
+print(f'Erreur linéaire quadratique moyenne en % = {round(mean_linear_squared_error_value_percent,2)} %')
+print('')
+print(f'Erreur polynomiale absolue moyenne flat = {round(mean_poly_absolute_error_value,2)}')
+print(f'Erreur polynomiale absolue moyenne en % = {round(mean_poly_absolute_error_value_percent,2)} %')
+print(f'Erreur polynomiale quadratique moyenne flat = {round(mean_poly_squared_error_value,2)}')
+print(f'Erreur polynomiale quadratique moyenne en % = {round(mean_poly_squared_error_value_percent,2)} %')
 
 # Affichage d'un diagrame résiduel pour savoir si les données sont adaptées à de la regression linéaire
-# test_residuals = y_test - test_predictions
+# test_residuals = y_test - test_linear_predictions
 # sns.scatterplot(x=y_test, y=test_residuals)
 # plt.axhline(y=0, color= 'r', ls='--')
 # plt.show()
